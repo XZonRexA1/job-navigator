c18d71bdbd1f703477bad81e488cad8711ea0177
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-04-12 13:19:17 +0600

    job details banner added

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./JobDetails.css";
 import { useLoaderData, useParams } from "react-router-dom";
+import vectorImage1 from "../../assets/All Images/Vector.png";
+import vectorImage2 from "../../assets/All Images/Vector-1.png";
 const JobDetails = () => {
   const { jobDetailsId } = useParams();
 
@@ -9,13 +11,19 @@ const JobDetails = () => {
   const [jobData, setJobData] = useState({});
   useEffect(() => {
     const newData = data?.find((db) => db.id === parseInt(jobDetailsId));
-    setJobData(newData)
+    setJobData(newData);
   }, []);
 
   console.log(jobData);
   return (
     <div>
-      <h2>Job Details</h2>
+      <div className="job-details-container">
+        <div className="img-container">
+          <img src={vectorImage1} alt="vector photo" />
+          <img src={vectorImage2} alt="vector photo" />
+        </div>
+        <h2 className="job-details-text">Job Details</h2>
+      </div>
     </div>
   );
 };
