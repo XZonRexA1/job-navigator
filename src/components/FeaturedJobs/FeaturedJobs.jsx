b7741992db78355a8a49cf224104db73dd765230
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-04-12 20:27:46 +0600

    show all button added

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,6 +3,9 @@ import "./FeaturedJobs.css";
 import FeaturedJob from "../FeaturedJob/FeaturedJob";
 const FeaturedJobs = () => {
   const [jobsData, setJobsData] = useState([]);
+
+  const [showAll, setShowAll] = useState(false);
+  const handleShowAll = () => setShowAll(true);
   useEffect(() => {
     fetch("jobData.json")
       .then((res) => res.json())
@@ -11,19 +14,29 @@ const FeaturedJobs = () => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>Featured Jobs</h1>
-      <p style={{ textAlign: "center", marginBottom: '30px' }}>
+      <p style={{ textAlign: "center", marginBottom: "30px" }}>
         Explore thousands of job opportunities with all the information you
         need. Its your future
       </p>
       <div className="jobs-container">
-        {jobsData.slice(0, 4).map((jobData) => (
+        {jobsData.slice(0, showAll ? 6 : 4).map((jobData) => (
           <FeaturedJob key={jobData.id} jobData={jobData}></FeaturedJob>
         ))}
       </div>
-      <div style={{display: "grid", justifyContent: "center", marginTop: '40px'}}>
-        <button style={{ textAlign: "center" }} className="btn-show-all">
-          See all Jobs
-        </button>
+      <div
+        style={{ display: "grid", justifyContent: "center", marginTop: "40px" }}
+      >
+        {!showAll && (
+          <span>
+            <button
+              onClick={handleShowAll}
+              style={{ textAlign: "center" }}
+              className="btn-show-all"
+            >
+              See all Jobs
+            </button>
+          </span>
+        )}
       </div>
     </div>
   );
